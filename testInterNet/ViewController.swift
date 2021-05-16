cee2083dde8611651309ed3db8a994244e587b87
Jpop <jjjjllears@gmail.com>
2021-05-16 19:23:52 +0800

    Initial Commit

---
+++ testInterNet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testInterNet
+//
+//  Created by 黃肇祺 on 2021/5/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
